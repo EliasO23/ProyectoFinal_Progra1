90b528bc05763112cb0f8ae237093d1e72804dcc
EliasO23 <124007307+EliasO23@users.noreply.github.com>
2023-05-08 19:53:11 -0600

    Proyecto

---
+++ AplicacionWebTiendaEnLinea/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AplicacionWebTiendaEnLinea.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AplicacionWebTiendaEnLinea.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Productos()
+        {
+            TiendaOnlineContext context = new TiendaOnlineContext();
+            var ListarProductos = context.Productos.ToList();
+
+            //ViewBag.Usuario = ListarUsuarios;
+
+            return View(ListarProductos);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
